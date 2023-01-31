53344526e85634cb41127e4a37be1aec5272687c
giulia <giuliar1@al.insper.edu.br>
2023-01-31 08:57:41 -0300

    carreguei os arquivos

---
+++ grupo04-giulia-luiza/sprites.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Input(pygame.sprite.Sprite):
+    def __init__(self, dicionario_de_arquivos, tamanho):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+
+        self.image = # carrega imagem
+        #todo objeto precisa de um rect
+        # rect é a representação de retangulo feita pelo pygame
+        self.rect = self.image.get_rect()
+        # é preciso definir onde a imagem deve aparecer no jogo
+        self.rect.centerx = # Define posicao x
+        self.rect.centery = # Define posicao y
+        
+
+class Botao(pygame.sprite.Sprite):
+    def __init__(self, assets, nome_do_jogo):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.assets = assets
+        self.image = assets['btn'] # assets é um dicionário de imagens, sons e fonts 
+        self.mask = pygame.mask.from_surface(self.image)
+        #todo objeto precisa de um rect
+        # rect é a representação de retangulo feita pelo pygame
+        self.rect = self.image.get_rect()
+        # é preciso definir onde a imagem deve aparecer no jogo
+        self.rect.x = 0
+        self.rect.y = 0
+
+        self.nome_do_jogo = nome_do_jogo
+
+    def mouse_over(self, over):
+        # Toda a lógica de movimentação deve ser feita aqui
+        # Atualização da posição da nave
+        if over:
+            self.image = self.assets['btn_hover']
+        else:
+            self.image = self.assets['btn']
+
+    
